d32cd6cad3db7bdd5732023959dd1e3074c11852
Vladislav Ganshin <ganshinv@gmail.com>
2019-12-28 16:24:23 +0300

    fix typo

--- src/fhir/migrations.clj
+++ src/fhir/migrations.clj
@@ -1,27 +1,23 @@
 (ns fhir.migrations)
 
 (def init "create table patient (
-    id text,
+    id text primary key,
     resource jsonb
 );
 
 create table encounter (
-    id text,
+    id text primary key,
     patient_id text,
     resource jsonb
 );
 
-create table practioner (
-    id text,
+create table practitioner (
+    id text primary key,
     resource jsonb
 );
 
-ALTER TABLE patient ADD PRIMARY KEY (id);
-ALTER TABLE practioner ADD PRIMARY KEY (id);
-
 SELECT create_distributed_table('patient', 'id');
 SELECT create_distributed_table('encounter', 'patient_id', colocate_with => 'patient');
-
 SELECT create_reference_table('practioner');
 ")
 
